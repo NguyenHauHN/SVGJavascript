a9306402de08e6dcd1613ce92b797f277c00b085
Nguyen Thi Hau <haunt@gmail.com>
2017-10-05 16:30:59 +0700

    create rectangle svg basic with svg.js and javascript

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 /**
  * Created by crawler on 05/10/2017.
  */
+
+// create svg with normal javascipt
+
+//  var uri = "http://www.w3.org/2000/svg";
+// var div = document.getElementById("drawing");
+// var svg = document.createElementNS(uri, "svg");
+// svg.setAttributeNS(null, "width", "100%");
+// svg.setAttributeNS(null, "height", "100%");
+// div.appendChild(svg);
+//
+// var rect = document.createElementNS(uri, "rect");
+// rect.setAttributeNS(null, "width", "100px");
+// rect.setAttributeNS(null, "height", "100px");
+// rect.setAttributeNS(null, "fill", "red");
+// svg.appendChild(rect);
+
+
+//create svg with svg.js
+var draw = SVG("drawing");
+var rect = draw.rect(100, 100).fill("red");
